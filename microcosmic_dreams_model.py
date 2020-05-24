482b8accb1d56924c0e5a9ce45e39de5d3f3a0d4
anwang427 <anna.wang2019@gmail.com>
2020-05-24 15:10:03 -0600

    model architecture updates

--- microcosmic_dreams_model.py
+++ microcosmic_dreams_model.py
@@ -43,13 +43,15 @@ y = np_utils.to_categorical(dataY)
 
 #define the LSTM model
 model = Sequential()
-model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
+model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences = True))
+model.add(Dropout(0.2))
+model.add(LSTM(256))
 model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
 #define the checkpoint
-filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+filepath="weights-improvement_2.0-{epoch:02d}-{loss:.4f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
-model.fit(X, y, epochs=20, batch_size=128, callbacks=callbacks_list)
+model.fit(X, y, epochs=50, batch_size=64, callbacks=callbacks_list)
